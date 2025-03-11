ad5baf387248f012713a1db941710d61bfa32730
Rehan16177 <rehan20072007385@gmail.com>
2025-03-11 18:39:47 +0100

    canvis en css

---
+++ BF3/E2/js/a1.js
@@ -0,0 +1,7 @@
+var respuesta = prompt('Tengo agujas pero no sé coser, tengo números pero no sé leer, las horas te doy, ¿Sabes quién soy?');
+
+if ( respuesta == "Reloj") {
+    console.log("¡Has acertado! Es el reloj.");
+} else {
+    console.log(" No has acertado. Intenta de nuevo.");
+}
