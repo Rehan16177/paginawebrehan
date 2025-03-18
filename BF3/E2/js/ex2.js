c4b0f6a4cd94426b764c6ee6e1fcb3116b8864ed
Rehan16177 <rehan20072007385@gmail.com>
2025-03-18 17:39:13 +0100

    PUJO ACT EXTRA2

---
+++ BF3/E2/js/ex2.js
@@ -0,0 +1,10 @@
+const pasword = '1234trex'
+const contrasena_guardada = prompt ("contraseña");
+if (pasword == contrasena_guardada.toLowerCase()) {
+    console.log("correct ");
+}
+else {
+    console.log("incorrect");
+}
+
+
