9b2efd215d676ca597c030a657fcef454dae338c
Rehan16177 <rehan20072007385@gmail.com>
2025-03-18 18:29:14 +0100

    activitat extra 3 js acabada

---
+++ BF3/E2/js/ex3.js
@@ -0,0 +1,11 @@
+const divisio = prompt ("Introduce el dividendo");
+
+const divisior = prompt ("Introduce el divisor");
+
+if (divisior == 0) {
+    console.log("incorrect ");
+}
+else {
+    console.log(divisio/divisior);
+}
+
