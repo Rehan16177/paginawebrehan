064a4fdd35266eec0374f64d710651f20df9cb9c
Rehan16177 <rehan20072007385@gmail.com>
2025-03-18 17:15:49 +0100

    activita extra 1 acabada

---
+++ BF3/E2/js/a3.js
@@ -0,0 +1,39 @@
+const animal1 = 'aguila';
+const sexo1 = 'hombre';
+const hijos1 = 7;
+
+if (sexo1 == 'mujer') {
+    var result = 'La mama ' + animal1 +  ' tiene ' +  hijos1 + ' hijos.';
+    console.log(result);
+} else {
+    var resultat = 'El padre ' + animal1 +  ' tiene ' +  hijos1 + ' hijos.';
+  console.log(resultat);
+}
+
+const animal2 = 'ballena';
+const sexo2 = 'mujer';
+const hijos2 = 1;
+
+if (sexo2 == 'mujer') {
+    var resulte = 'La mama ' + animal2 +  ' tiene ' +  hijos2 + ' hijos.';
+    console.log(resulte);
+} else {
+    var resultats = 'El padre ' + animal2 +  ' tiene ' +  hijos2 + ' hijos.';
+  console.log(resultats);
+}
+
+const animal3 = 'tigre';
+const sexo3 = 'hombre';
+const hijos3 = 0;
+
+if (sexo3 == 'mujer') {
+    var resultes = 'La mama ' + animal3 +  ' tiene ' +  hijos3 + ' hijos.';
+    console.log(resultes);
+} else {
+    var resultatss = 'El padre ' + animal3 +  ' tiene ' +  hijos3 + ' hijos.';
+  console.log(resultatss);
+}
+
+
+
+
---
+++ BF3/E2/js/ex.js
@@ -0,0 +1,9 @@
+const age = prompt("Cuantos AÑOS TIENES.");
+
+
+if (age >= 18) {
+    console.log("ERES MAYOR DE EDAD");
+}
+else {
+    console.log("NO ERES MAYOR DE EDAD");
+}
