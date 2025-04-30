1f7a4d8149b277704cd27c5a5c79ecd3954e3ca4
Rehan16177 <rehan20072007385@gmail.com>
2025-04-30 17:36:24 +0200

    canvis js y algunes act fetes

---
+++ BF3/E7/js/a3.js
@@ -0,0 +1,18 @@
+function calcular() {
+    var i1 = document.getElementById('numero1');
+    var i2 = document.getElementById('numero2');
+    var num1 = parseInt(i1.value);
+    var num2 = parseInt(i2.value);
+    var res = num1 + num2;
+    var pres = document.getElementById('resultat');
+    pres.innerHTML = res;
+
+
+    
+
+
+}
+
+    
+    
+
---
+++ BF3/E7/js/a4.js
@@ -0,0 +1,9 @@
+var counter = 0;
+
+function notific(){
+    counter++;
+    var noti = document.getElementById('notificacion');
+    noti.innerText = counter;
+
+
+}
